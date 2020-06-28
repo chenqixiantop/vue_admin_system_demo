e73963816bbd396886c0777a70ca7eeb381b899e
chenqixian <chenqixian@96900.com.cn>
2020-06-28 21:25:00 +0800

    提交第一版vue-cli3

---
+++ vue_admin_system/babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // Babel 是一个工具链，主要用于将 ECMAScript 2015+ 版本的代码转换为向后兼容的 JavaScript 语法，以便能够运行在当前和旧版本的浏览器或其他环境中
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ],
+  'env': {
+    'development': {
+      // babel-plugin-dynamic-import-node plugin only does one thing by converting all import() to require().
+      // This plugin can significantly increase the speed of hot updates, when you have a large number of pages.
+      // https://panjiachen.github.io/vue-element-admin-site/guide/advanced/lazy-loading.html
+      'plugins': ['dynamic-import-node']
+    }
+  }
+}
